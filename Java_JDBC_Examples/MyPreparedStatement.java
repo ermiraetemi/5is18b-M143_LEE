b0cdb7ca5724b40f36ec9339d21259e95d6dbd99
caddywoof <66834679+caddywoof@users.noreply.github.com>
2020-06-25 21:20:36 +0200

    MyPreparedStatement.java LB
    lol7

--- Java_JDBC_Examples/MyPreparedStatement.java
+++ Java_JDBC_Examples/MyPreparedStatement.java
@@ -20,7 +20,7 @@ public class MyPreparedStatement {
             prSt = con.prepareStatement(query);
             prSt.setString(1, "John");
             prSt.setInt(2, 10000);
-            //count will give you how many records got updated
+            //count will give you how many records got updated Leonie LoL Beyer :-P
             int count = prSt.executeUpdate();
             //Run the same query with different values
             prSt.setString(1, "Cric");
